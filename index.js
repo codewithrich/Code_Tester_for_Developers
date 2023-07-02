530b9174693892cbe443db1a771bdb3b86614a46
Richardson <104580727+codewithrich@users.noreply.github.com>
2023-07-02 21:28:03 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+// Using except function
+var expect =(val) => {
+    return{
+        toBe: (val2)=> {
+            if( val !== val2) throw new Error("Not Equal");
+            else return true;
+        },
+        notToBe: (val2) => {
+            if (val  === val2) throw new Error ('Equal');
+            else return true;
+        }
+    }
+};
